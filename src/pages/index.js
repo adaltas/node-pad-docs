e2c8995759faa47a21852e83928571f23506bf09
serg <sergkudinov@gmail.com>
2019-05-06 16:56:13 +0200

    index: remove extra depth of div

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,35 +36,32 @@ pad(5, 'pad', '+') // "++pad"
 `.trim()
 
 const styles = {
+  homePage: {
+    " section h2": {
+      textAlign: "center",
+      paddingTop: 0,
+    },
+  },
   featureContainer: {
     maxWidth: 600,
     margin: "auto",
+    position: "relative",
     "> div": {
-      padding: "2rem 0",
-      "> div": {
-        width: "50%",
-        display: "inline-block",
-        padding: "1rem 2rem 1rem 3rem",
-        position: "relative",
-        verticalAlign: "top",
-        "> div": {
-          position: "absolute",
-          width: "2.5rem",
-          height: "2.5rem",
-          top: ".5rem",
-          left: "0rem",
-        },
-        "> div > img": {
-          width: "100%",
-          verticalAlign: "bottom",
-        },
-        "@media (max-width: 768px)": {
-          width: "100%",
-        },
-      },
+      padding: "2.5rem 0",
+      width: "50%",
+      display: "inline-block",
       "@media (max-width: 768px)": {
-        padding: "0",
+        width: "100%",
+        padding: "1rem 0",
       },
+      "> img": {
+        width: "2.5rem",
+        verticalAlign: "middle",
+        marginRight: ".5rem",
+      },
+    },
+    "@media (max-width: 768px)": {
+      padding: "0",
     },
   },
 }
@@ -75,54 +72,34 @@ const IndexPage = () => (
       title="PAD for Node.js"
       keywords={[`gatsby`, `application`, `react`]}
     />
-    <div className={"home-page"}>
+    <div className={css(styles.homePage)}>
       <section>
         <div>
           <h2>Key features</h2>
           <div className={css(styles.featureContainer)}>
             <div>
-              <div>
-                <div>
-                  <img src={IconApi} alt="Simple API" />
-                </div>
-                <span>Simple API</span>
-              </div>
-              <div>
-                <div>
-                  <img src={IconLight} alt="Light weight" />
-                </div>
-                <span>
-                  Light weight, 1 dependency
-                </span>
-              </div>
+              <img src={IconApi} alt="Simple API" />
+              <span>Simple API</span>
             </div>
             <div>
-              <div>
-                <div>
-                  <img src={IconBundles} alt="CJS, ESM and UMD bundles" />
-                </div>
-                <span>CJS, ESM and UMD bundles</span>
-              </div>
-              <div>
-                <div>
-                  <img src={IconDocs} alt="Documented and tested" />
-                </div>
-                <span>Documented and tested</span>
-              </div>
+              <img src={IconLight} alt="Light weight" />
+              <span>Light weight, 1&nbsp;dependency</span>
             </div>
             <div>
-              <div>
-                <div>
-                  <img src={IconMoustache} alt="Well tested, mature" />
-                </div>
-                <span>Well tested, mature</span>
-              </div>
-              <div>
-                <div>
-                  <img src={IconLicense} alt="MIT License" />
-                </div>
-                <span>MIT License</span>
-              </div>
+              <img src={IconBundles} alt="CJS, ESM and UMD bundles" />
+              <span>CJS, ESM and&nbsp;UMD bundles</span>
+            </div>
+            <div>
+              <img src={IconDocs} alt="Documented and tested" />
+              <span>Documented and&nbsp;tested</span>
+            </div>
+            <div>
+              <img src={IconMoustache} alt="Well tested, mature" />
+              <span>Well tested, mature</span>
+            </div>
+            <div>
+              <img src={IconLicense} alt="MIT License" />
+              <span>MIT License</span>
             </div>
           </div>
         </div>
